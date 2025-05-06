301ffae09d81e732e237d69d372e4509877ccbea
YouMuse <politevisitor@msn.cn>
2025-05-06 17:37:45 +0800

    Initialization

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import inbox from './components/inbox.vue'
+import discord from './components/discord.vue'
+</script>
+
+<template>
+  <discord/>
+</template>
